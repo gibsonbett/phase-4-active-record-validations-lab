34caf496432fc6560ecf4e2e28d1aad7c808de67
Gibson-Bett <gibubett@gmail.com>
2022-11-29 20:37:18 +0300

    completed assignment

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,15 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: {minimum:250}
+    validates :summary, length: {maximum:250}
+    validates  :category, inclusion: {in: %w(Fiction Non-Fiction)}
+    validate :title_is_clickbait?
+
+    CLICKBAITS = [ /You Won't Believe These /i ]
+    
+    def title_is_clickbait?
+        if CLICKBAITS.none? {|t| t.match title}
+            errors.add(:title, "must be a clickbait")
+        end
+    end
 end
